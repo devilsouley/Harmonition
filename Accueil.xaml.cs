75d72a23fbb76eb2483e327c050ebd95ee9a406f
devilsouley <marcilpa@hotmail.com>
2015-10-01 11:17:55 -0400

    upd Pat

---
+++ Accueil.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Harmonition
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class Accueil : Window
+    {
+        public WMPLib.WindowsMediaPlayer wplayer;
+        static readonly object locker = new object();
+
+        public Accueil()
+        {
+            InitializeComponent();
+
+            wplayer = new WMPLib.WindowsMediaPlayer();
+            wplayer.URL = "H:/Bureau/Harmonition - Accueil/Harmonition - Accueil/FichiersAudio/ValleeGerudo.mp3";
+            
+            
+            wplayer.controls.play();
+            
+
+        }
+
+        private void UpdateDureeCourante(Action callback)
+        {
+            //lblDureeCourante.Content = wplayer.controls.currentPositionString;
+
+            Thread.Sleep(1000);
+            callback();
+        }
+
+        // TODO
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            new Thread(() => UpdateDureeCourante(() => lblDureeCourante.Content = wplayer.controls.currentPositionString)).Start();
+
+
+            /*
+            while (wplayer.controls.currentPosition < wplayer.currentMedia.duration)
+            {
+                lock(locker)
+                {
+                    new Thread(UpdateDureeCourante).Start();
+                    UpdateDureeCourante();
+                }
+            }
+            */
+            
+        }
+
+    }
+}
